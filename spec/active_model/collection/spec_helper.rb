5f70693264df441c560db6cf659201ff5b5f76f4
sleepingkingstudios <sleeping.king.studios@gmail.com>
2014-06-23 01:25:25 -0400

    Add spec_helper for RSpec testing.
    Current spec status: 0 examples, 0 failures in in 0.00021 seconds.

---
+++ spec/active_model/collection/spec_helper.rb
@@ -0,0 +1,41 @@
+# spec/active_model/collection/spec_helper.rb
+
+require 'rspec'
+require 'factory_girl'
+require 'pry'
+
+#=# Require Factories, Custom Matchers, &c #=#
+Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each { |f| require f }
+
+RSpec.configure do |config|
+  # Limit a spec run to individual examples or groups by tagging them with
+  # `:focus` metadata.
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = "random"
+  Kernel.srand config.seed
+
+  # Alias "it should behave like" to 2.13-like syntax.
+  config.alias_it_should_behave_like_to 'expect_behavior', 'has behavior'
+
+  # rspec-expectations config goes here.
+  config.expect_with :rspec do |expectations|
+    # Enable only the newer, non-monkey-patching expect syntax.
+    expectations.syntax = :expect
+  end # expect_with
+
+  # rspec-mocks config goes here.
+  config.mock_with :rspec do |mocks|
+    # Enable only the newer, non-monkey-patching expect syntax.
+    mocks.syntax = :expect
+
+    # Prevents you from mocking or stubbing a method that does not exist on
+    # a real object. This is generally recommended.
+    mocks.verify_partial_doubles = true
+  end # mock_with
+end # config
